d49d015c038be303a92dcc3ddaf10a92048b2ef3
Barrett Clark <barrett.clark@hashicorp.com>
2022-11-09 06:39:26 -0600

    Update to Rails 7.x

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
+Rails.application.routes.draw do
   get 'solver/index'
   post 'solver/letters'
   root 'solver#index'
